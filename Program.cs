165a75633aeb4f64d8ebab83cb5bf054bac5f8a3
MarioMW <92758562+MarioMW@users.noreply.github.com>
2021-10-18 17:29:02 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ejercicio_practico_1er_corte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valora;
+            int valorb;
+            int valorc;
+
+            valora = 15;
+            valorb = 50;
+            valorc = 23;
+
+            Console.WriteLine("Combinacion de variables en orden aacb");
+            Console.Write(valora);
+            Console.Write(valora);
+            Console.Write(valorc);
+            Console.WriteLine(valorb);
+            Console.WriteLine("--------");
+
+
+            Console.WriteLine("Combinacion de variables en orden bca");
+            Console.Write(valorb);
+            Console.Write(valorc);
+            Console.WriteLine(valora);
+            Console.WriteLine("--------");
+
+            Console.WriteLine("Combinacion de variables en orden bcbc");
+            Console.Write(valorb);
+            Console.Write(valorc);
+            Console.Write(valorb);
+            Console.WriteLine(valorc);
+            Console.WriteLine("--------");
+        }
+    }
+}
