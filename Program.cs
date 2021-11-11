3e79a5202413ab304637ae952b2f023bf31313a3
MarioMW <92758562+MarioMW@users.noreply.github.com>
2021-11-11 13:28:13 -0400

    Add files via upload
    Ejercicio practico segundo corte

--- Program.cs
+++ Program.cs
@@ -1,39 +1,44 @@
 ﻿using System;
 
-namespace ejercicio_practico_1er_corte
+namespace ejercicio_practico_2do_corte
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int valora;
-            int valorb;
-            int valorc;
-
-            valora = 15;
-            valorb = 50;
-            valorc = 23;
-
-            Console.WriteLine("Combinacion de variables en orden aacb");
-            Console.Write(valora);
-            Console.Write(valora);
-            Console.Write(valorc);
-            Console.WriteLine(valorb);
-            Console.WriteLine("--------");
-
-
-            Console.WriteLine("Combinacion de variables en orden bca");
-            Console.Write(valorb);
-            Console.Write(valorc);
-            Console.WriteLine(valora);
-            Console.WriteLine("--------");
-
-            Console.WriteLine("Combinacion de variables en orden bcbc");
-            Console.Write(valorb);
-            Console.Write(valorc);
-            Console.Write(valorb);
-            Console.WriteLine(valorc);
-            Console.WriteLine("--------");
+            string cadena ="";
+            int edad = 0;
+            int altura = 0;
+            
+            
+            Console.WriteLine("¡Hola! Bienvenido a Disneyland, por favor indicanos tu edad");
+            cadena = Console.ReadLine();
+            edad = Convert.ToInt32(cadena);
+            
+            
+            if(edad > 16)
+            {
+                Console.WriteLine("Disculpe ha superado el limite de edad para la seccion de juegos infantiles");
+            }
+            else
+            {
+                
+                
+                Console.WriteLine("Bienvenido, puedes ingresar a la seccion de juegos infantiles");
+                
+                Console.WriteLine("Ahora, ¿Podrías indicarnos tu estatura? en centimetros por favor");
+                cadena = Console.ReadLine();
+                altura = Convert.ToInt32(cadena);
+                
+                if(altura < 160)
+                {
+                Console.WriteLine("Puedes ir Dumbo The Flying Elephant, Cinderella Castle y It´s a small world");    
+                }
+                else
+                {
+                    Console.WriteLine("Puedes ir a todos los juegos del parque");
+                }
+            }
         }
     }
-}
+}   
